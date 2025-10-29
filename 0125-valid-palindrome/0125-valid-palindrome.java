293a6868cfb96a25ca866d362403303456570f49
Vishisht_Dwivedi <vishisht22868@gmail.com>
2025-10-29 12:21:14 +0530

    Time: 1 ms (100%), Space: 43.3 MB (57.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String reduced = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        if(reduced.length()==0) return true;
-        for(int i=0;i<reduced.length()/2;i++){
-            if(reduced.charAt(i)!=reduced.charAt(reduced.length()-i-1)){
-                return false;
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                arr[i] = (char)(arr[i] + 32);
             }
         }
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            while (start < end && !isAlphaNum(arr[start])) {
+                start++;
+            }
+            while (start<end&&!isAlphaNum(arr[end])) {
+                end--;
+            }
+            if (arr[start] != arr[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
         return true;
+    }
+    public boolean isAlphaNum(char c) {
+        return (c>='a'&&c<='z')||(c>='0'&&c<='9');
     }
 }
