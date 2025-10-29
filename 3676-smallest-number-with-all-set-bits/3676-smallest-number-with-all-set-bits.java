74482b4bd4dfd0067c271a1babbb395cc355bb02
Vishisht_Dwivedi <vishisht22868@gmail.com>
2025-10-29 16:14:50 +0530

    Time: 0 ms (100%), Space: 41 MB (36.78%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,12 +1,12 @@
 class Solution {
     public int smallestNumber(int n) {
-        int iter = 0;
-        int finalRes = 0;
+        int finalRes = 1;
+        int iter = 1;
         while(n>0){
             n/=2;
-            finalRes+=Math.pow(2,iter);
-            iter++;
+            finalRes += iter*2;
+            iter*=2;
         }
-        return finalRes;
+        return finalRes/2;
     }
 }
